b2a088c47854a63d3ac06084097f91e684956983
helloduk <helloduk@gmail.com>
2018-11-06 17:57:48 +0900

    Complete
    v1.0 complete

--- app/src/main/java/com/solluzfa/solluzviewer/model/MachineData.kt
+++ app/src/main/java/com/solluzfa/solluzviewer/model/MachineData.kt
@@ -6,9 +6,13 @@ import io.reactivex.Observable
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import io.reactivex.android.schedulers.AndroidSchedulers
+import java.io.BufferedReader
+import java.io.InputStreamReader
 import java.net.URL
 import java.net.URLDecoder
 import java.net.URLEncoder
+import java.nio.ByteBuffer
+import java.nio.charset.Charset
 import java.util.concurrent.TimeUnit
 
 
@@ -16,20 +20,20 @@ class MachineData(var name: MutableLiveData<String>, var state: MutableLiveData<
                   var passed: MutableLiveData<Int>, var failed: MutableLiveData<Int>,
                   var total: MutableLiveData<Int>, var passPercentage: MutableLiveData<Float>) {
     private val TAG = "MachineData"
-    private val RETRY_COUNT = 10L
     private val ERROR_PACKET = "N:File error,S:N,G:1,N:1"
-
+    private val UrlAddress = URL("http://solluz.iptime.org/Data/MachineData1.txt");
+    //private val UrlAddress = URL("https://helloduk.github.io/MachineData1.txt");
     lateinit var disposable: Disposable
 
     var dataObservable : Observable<String>
 
     init {
-        val UrlAddress = URL("http://solluz.iptime.org/Data/MachineData1.txt");
-
         dataObservable = Observable.interval(1, TimeUnit.SECONDS)
                 .subscribeOn(Schedulers.io())
                 .switchMap { _ ->
-                    Observable.just(UrlAddress.readText(Charsets.UTF_8))
+                    //Original file is encoded by "euc-kr"
+                    Observable.just(UrlAddress.readText(Charset.forName("euc-kr")))
+                    //Observable.just(readData())
                 }
                 .doOnError {e -> run {
                     Log.e(TAG, e.toString())
@@ -40,6 +44,13 @@ class MachineData(var name: MutableLiveData<String>, var state: MutableLiveData<
                 .retry()
                 .doOnNext { data -> Log.i(TAG, data) }
                 .observeOn(AndroidSchedulers.mainThread())
+    }
+
+    // for reference. another method to get text.
+    private fun readData() : String {
+        //Original file is encoded by "euc-kr"
+        val reader = BufferedReader(InputStreamReader(UrlAddress.openStream(), "euc-kr"))
+        return reader.readLine()
     }
 
     fun showState() {
